828abbe4916f7b7acc40f8735d7d98028023582a
Abioye Kazeem <babatundekazeem200@gmail.com>
2023-05-09 08:30:41 +0100

    Eight Challenge First Commit
    This challenge is centered on Join usage in SQL (Inner Join, Outer Join(left, right and full outer join)).

---
+++ eightchallenge.sql
@@ -0,0 +1,76 @@
+USE SAMPLEDB
+
+GO
+
+SELECT *
+FROM hcm.departments;
+
+
+-- This query the total number of row on department_id column
+SELECT Count(*) AS row_count,
+Count(DISTINCT department_id) AS distinct_row_count
+FROM hcm.departments;
+
+-- Query information schema views to get metadata on contraints on hcm.departments table
+SELECT
+tc.TABLE_SCHEMA,
+tc.TABLE_NAME,
+tc.CONSTRAINT_TYPE,
+ccu.COLUMN_NAME
+FROM 
+INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc
+JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE ccu
+ON tc.CONSTRAINT_NAME = ccu.CONSTRAINT_NAME
+AND tc.TABLE_NAME = ccu.TABLE_NAME
+AND tc.TABLE_SCHEMA = ccu.TABLE_SCHEMA
+WHERE TC.TABLE_SCHEMA = 'hcm' AND tc.TABLE_NAME ='departments';
+
+---------------------------------------------------------------------------------------------------------------------------------------
+SELECT *
+FROM hcm.departments
+
+SELECT * 
+FROM hcm.employees;
+
+-- Challenge1
+-- Write a querry  to return the following attrributes for employees who belong to a department
+-- employee_id
+-- first_name
+-- last_name
+-- salary
+-- department_name
+-- Hint: Get department name from hcm.departments table. Get all other attributes from hcm.employees table. Use Inner Join btw the two tables
+-- This is cos u are to only include employees who only belong to a department.
+SELECT
+d.department_id,d.department_name, e.employee_id, e.first_name, e.last_name, e.salary
+FROM hcm.departments d INNER JOIN hcm.employees e
+ON d.department_id = e.employee_id;
+
+-------------------------------------------------------------------------------------------------------------------------
+-- Challenge2
+-- Write a querry  to return the following attrributes for all employees, including employees who do not belong to a department
+-- employee_id
+-- first_name
+-- last_name
+-- salary
+-- department_name
+-- Hint: Get department name from hcm.departments table. Get all other attributes from hcm.employees table. Use Outer Join btw the two tables
+-- This is cos u are to only include all employees including the ones who does not belong to any department.
+-- NOTE: I used FULL OUTER JOIN here cos am to returns all the required attributes of all employees with department or without department
+SELECT
+d.department_id, d.department_name, e.employee_id, e.first_name, e.last_name, e.salary
+FROM hcm.departments d FULL OUTER JOIN hcm.employees e
+ON d.department_id = e.employee_id;
+
+--------------------------------------------------------------------------------------------------------------------------
+-- Challenge3
+-- Write a query to return the total number of employees in each department, include the department_name in the query result.
+-- also include employees who have not been assigned to a department.
+-- Hint: Use an outer join as well as GROUP BY clause
+SELECT 
+d.department_name,Sum(e.employee_id) AS Total_employees_in_each_dept
+FROM hcm.departments d FULL OUTER JOIN hcm.employees e
+ON d.department_id = e.employee_id
+GROUP BY d.department_name;
+
+---------------------------------------------------------------------------------------------------------------------------
